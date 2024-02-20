c64c0ca18c85ce5c0df0285d3ee5bdb35254b04b
wobsoriano <sorianorobertc@gmail.com>
2024-02-20 00:54:50 -0800

    add shadcn-vue for example

--- dev/vite.config.ts
+++ dev/vite.config.ts
@@ -1,13 +1,21 @@
 import path from 'node:path'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import tailwind from 'tailwindcss'
+import autoprefixer from 'autoprefixer'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
   resolve: {
     alias: {
+      "@": path.resolve(__dirname, './src'),
       'vue-input-otp': path.resolve(__dirname, '../src'),
+    },
+  },
+  css: {
+    postcss: {
+      plugins: [tailwind(), autoprefixer()],
     },
   },
 })
