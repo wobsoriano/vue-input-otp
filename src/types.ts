dc853d1c2ab44f0df79b28bcd209ce5583b13197
wobsoriano <sorianorobertc@gmail.com>
2024-02-20 17:12:14 -0800

    update types

--- src/types.ts
+++ src/types.ts
@@ -1,3 +1,49 @@
+// Need to figure out how to import this from Vue without causing "Failed to resolve extends base type" error
+export interface InputHTMLAttributes {
+  accept?: string
+  alt?: string
+  autocomplete?: string
+  autofocus?: boolean
+  capture?: boolean | 'user' | 'environment'
+  checked?: boolean | any[] | Set<any>
+  crossorigin?: string
+  disabled?: boolean
+  enterKeyHint?: 'enter' | 'done' | 'go' | 'next' | 'previous' | 'search' | 'send'
+  form?: string
+  formaction?: string
+  formenctype?: string
+  formmethod?: string
+  formnovalidate?: boolean
+  formtarget?: string
+  height?: number
+  indeterminate?: boolean
+  list?: string
+  max?: number
+  maxlength?: number
+  min?: number
+  minlength?: number
+  multiple?: boolean
+  name?: string
+  pattern?: string
+  placeholder?: string
+  readonly?: boolean
+  required?: boolean
+  size?: number
+  src?: string
+  step?: number
+  type?: string
+  value?: any
+  width?: number
+}
+
+export interface OTPInputProps extends InputHTMLAttributes {
+  modelValue?: string
+  maxlength: number
+  allowNavigation?: boolean
+  inputmode?: 'numeric' | 'text'
+  containerClass?: string
+}
+
 export enum SelectionType {
   CARET = 0,
   CHAR = 1,
--- website/src/components/Slot.vue
+++ website/src/components/Slot.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
-import { cn } from '@/lib/utils';
 import { computed } from 'vue'
+import { cn } from '@/lib/utils'
 
 const props = defineProps<{
   char: string | null
